45cde83943ce067ca2982ee269bec0246d4ef9ad
unknown <20130702126@neoway.com.cn>
2017-06-16 17:42:01 +0800

    add login process

--- login/tsdm/tsdm.py
+++ login/tsdm/tsdm.py
@@ -9,16 +9,17 @@ import pytesseract
 from PIL import Image
 import re
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 '
-                         '(KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-           }
-
-# 全局的session
-session = requests.session()
-session.get('http://www.tsdm.me', headers=headers)
-
 
 class TSDM:
+    session = requests.session()
+
+    def __init__(self):
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 '
+                                 '(KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+                   }
+
+        # 全局的session
+        self.session.get('http://www.tsdm.me', headers=headers)
 
     def getCaptcahImage(self, saveImageFilePath=''):
         headersForCaptcha = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 '
@@ -29,7 +30,7 @@ class TSDM:
                              'Accept-Encoding': 'gzip, deflate, sdch',
                              'Accept-Language': 'zh-CN,zh;q=0.8'
                              }
-        resp = session.get(url='http://www.tsdm.me/plugin.php?id=oracle:verify', headers=headersForCaptcha)
+        resp = self.session.get(url='http://www.tsdm.me/plugin.php?id=oracle:verify', headers=headersForCaptcha)
         if resp.status_code == 200:
             with open(saveImageFilePath, 'wb') as file:
                 file.write(resp.content)
@@ -48,22 +49,62 @@ class TSDM:
                              'Accept-Encoding': 'gzip, deflate, sdch',
                              'Accept-Language': 'zh-CN,zh;q=0.8'
                              }
-        resp = session.get(url='http://www.tsdm.me/member.php?mod=logging&action=login', headers=headersForHomePage)
+        resp = self.session.get(url='http://www.tsdm.me/member.php?mod=logging&action=login', headers=headersForHomePage)
         if resp.status_code == 200:
             return resp.content.decode(resp.encoding)
         return ''
 
-    def __getHashCodeFromPage(self):
+    def getHashCodeFromPage(self):
         result = {'loginhash': '', 'formhash': ''}
         html = self.getLoginHomePage()
         if len(html) == 0:
             return result
 
+        loginhashPattern = re.compile('<div id="main_messaqge_([0-9a-zA-Z]+)">')
+        formhashPattern = re.compile('<input type="hidden" name="formhash" value="([0-9a-zA-Z]+)" />')
+        lines = html.split('\n')
+        for line in lines:
+            if len(line) == 0:
+                continue
+            line = line.strip()
+            if len(line) == 0:
+                continue
 
+            match = loginhashPattern.match(line)
+            if match:
+                result['loginhash'] = match.group(1)
 
-    def login(self):
-        pass
+            match = formhashPattern.match(line)
+            if match:
+                result['formhash'] = match.group(1)
+        return result
 
+    def login(self, userName, password, captcha, loginhash, formhash):
+        headersForLogin= {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 '
+                                            '(KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+                              'Origin': 'http://www.tsdm.me',
+                          'Referer': 'http://www.tsdm.me/member.php?mod=logging&action=login',
+                              'Upgrade-Insecure-Requests': '1',
+                              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                              'Accept-Encoding': 'gzip, deflate, sdch',
+                              'Accept-Language': 'zh-CN,zh;q=0.8',
+                          'Content-Type': 'application/x-www-form-urlencoded'
+                              }
+        formData = {
+            'formhash': formhash,
+            'referer': 'http://www.tsdm.me/./',
+            'loginfield': 'username',
+            'username': userName,
+            'password': password,
+            'tsdm_verify': captcha,
+            'questionid': 0,
+            'answer': ''
+
+        }
+        resp = self.session.post(url='http://www.tsdm.me//member.php?mod=logging&action=login&loginsubmit=yes&loginhash={0}&inajax=1'.format(loginhash),
+                           headers=headersForLogin,
+                           data=formData)
+        return resp.status_code == 200
 
 if __name__ == '__main__':
     tsdm = TSDM()
